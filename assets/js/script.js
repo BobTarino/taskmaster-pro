15a356eb0008efed675f12a731da0cc5b35047d4
Bobby T <bob.tarino@gmail.com>
2021-04-17 16:07:15 -0500

    convert list into sortables

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,7 +123,7 @@ $("#task-form-modal .btn-primary").click(function () {
 });
 
 // due date was clicked
-$(".list-group").on("click", "span", function() {
+$(".list-group").on("click", "span", function () {
   // get current text
   var date = $(this)
     .text()
@@ -142,7 +142,7 @@ $(".list-group").on("click", "span", function() {
   dateInput.trigger("focus");
 });
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("blur", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
@@ -183,5 +183,58 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+//make list groups sortable using conectWith selector
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  // create a copy of dragged element 
+  helper: "clone",
+  activate: function (event) {
+    // console.log("activate", this);
+  },
+  deactivate: function (event) {
+    // console.log("deactivate", this);
+  },
+  over: function (event) {
+    // console.log("over", event.target);
+  },
+  out: function (event) {
+    // console.log("out", event.target);
+  },
+  update: function (event) {
+    // $(this) refers to the child element at that index
+    // array to store the task data in
+    var tempArr = [];
 
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    console.log(tempArr);
+  }
+});
 
